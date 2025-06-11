758184c2169600ebf75549710604cdd13fd5f45a
Francisco Rugiero <pacorugierom@gmail.com>
2025-06-11 16:13:48 -0300

    Modificaciones

---
+++ cliente/migrations/0003_alter_pais_nombre.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cliente', '0002_pais_cliente_nacimiento_cliente_pais_origen'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='pais',
+            name='nombre',
+            field=models.CharField(max_length=50, unique=True),
+        ),
+    ]
